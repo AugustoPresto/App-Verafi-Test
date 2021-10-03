88267fcc366e632222eec0d5995dd13e140f5507
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-10-02 21:03:13 -0300

    Added validations and just two possible statuses, UP and DOWN

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,4 +1,9 @@
 class Server < ApplicationRecord
   serialize :status, Array
   serialize :message, Array
+
+  ALLOWED_STATUSES = ["UP", "DOWN"]
+
+  validates :status, :message, presence: true
+  validates :status, inclusion: { in: ALLOWED_STATUSES, message: "%{value} is not valid" }
 end
